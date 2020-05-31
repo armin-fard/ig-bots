890ee449373dc16dcb71686c047ebc7f1dd74c2f
armin-fard <afarshidfard@gmail.com>
2020-05-31 10:23:24 -0700

    username/password elements found by css_selector method

--- 001_bot.py
+++ 001_bot.py
@@ -2,8 +2,17 @@ from time import sleep
 from selenium import webdriver
 
 browser = webdriver.Firefox()
+brower.implicitly_wait(5)
 
 browser.get('https://www.instagram.com/')
+
+login_link = browser.find_element_by_xpath("//a[text()='Log in']")
+login_link.click()
+
+sleep(2)
+
+username_input = browser.find_element_by_css_selector("input[name='username']")
+username_input = browser.find_element_by_css_selector("input[name='password']")
 
 sleep(5)
 
